8e9ae168b9e8712be38d302b8c071d3192073025
Hasnat Amin <hasnatamin065@gmail.com>
2021-04-09 23:55:39 +0500

    updating the app

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,81 @@
-import React,{ReactDOM}from 'react'
+import React,{useState, ReactDOM}from 'react'
 import './Home.css'
-import addHistroy from './addHistroy'
-class  Home extends React.Component{
-    state = {
-        'discription': "",
-        'current_balance' : 0,
-        'income' : 0,
-        'expenses' : 0,
+import AddHistroy from './addHistroy'
+function Home() {
+    let [TransactionList, SetTransactionList] = useState([]);
+    let [current_balance,setcurrent_balance] = useState(0);
+    let [expenses,setexpenses] = useState(0);
+    let [income,setincome] = useState(0);
+
+    let [textInput,settextInput] = useState("");
+    let [amount, setamount]= useState(0);
     
-    }
-    
-    submitbtnclick = (event) => {
-        
-        if(this.refs.textInput !== null && this.refs.amount !== null){
+    let submitbtnclick = (e) => {
+        if(textInput !== ''){
             
-            let disValue = this.refs.textInput.value
-            let amountValue = this.refs.amount.value
-            this.setState({discription:disValue})
-           
+            let disValue = textInput
+            let amountValue = parseInt(amount)
+            console.log(disValue);
+            console.log(amountValue);
+            let TempList = {
+                desc : disValue,
+                amount : amountValue
+            }
+            SetTransactionList([...TransactionList, TempList])
             if(amountValue  < 0 ){
-                let b = this.state.expenses- Math.abs(amountValue)
-                this.setState({expenses: b})
-                let c =  this.state.current_balance - Math.abs(amountValue)
-                this.setState({current_balance : c})
+                let b = expenses- Math.abs(amountValue)
+                setexpenses(b)
+                console.log("Expenses",expenses)
+                let c =  current_balance - Math.abs(amountValue)
+                setcurrent_balance(c)
+                console.log("current_balance",current_balance)
             }
             
             else{
-                let ab = this.state.income
+                let ab = income
                 let a = Math.abs(amountValue) + ab
-                this.setState({income: a})
-                let c =  this.state.current_balance + Math.abs(amountValue)
-                this.setState({current_balance : c})
+                setincome(a)
+                console.log("income",income)
+
+                let c =  current_balance + Math.abs(amountValue)
+                setcurrent_balance(c)
+                console.log("current_balance",current_balance)
+
             }
+            settextInput("");
+            setamount(0);
             //ReactDOM.render(<addHistroy discription={disValue} amount={amountValue} />);
             //<addHistry discription={disValue} amount={amountValue}/>
             //<li><span>{disValue}</span> <span>{amountValue}</span></li>
         }
+        else{
+            alert("Please fill the Input fields")
+        }
         
     }
-    render(){
+    console.log(textInput);
+    console.log(amount);
         return(
             <div className="whole">
                 <div className='whole1'>
                 <h1>Expense Tracker APP</h1>
                 <div className='part1'>
-                    <h3>Your current Balance : <span>{this.state.current_balance}</span></h3>
+                    <h3>Your current Balance <br></br> <span>{current_balance}</span></h3>
                     <div className="details">
-                        <h4>Your Income : <span>{this.state.income}</span></h4>
-                        <h4>Your Expenses : <span>{this.state.expenses}</span></h4>
+                        <h4>Your Income  <br></br><span>{income}</span></h4>
+                        <h4>Your Expenses  <br></br> <span>{expenses}</span></h4>
                     </div>
-                    {/* <h4>History</h4> */}
-                    <ul className='list'>
-                    
-                    </ul>
+                        <h4>History</h4> 
+                       <div className="cont"> {
+                            TransactionList.map((item, index)=>
+                                <AddHistroy TransactionList={TransactionList} SetTransactionList={SetTransactionList} item={item} desc={item.desc} amount={item.amount} index={index}/>
+                            )
+                        }
+                    </div>
                     <h4>Add Transaction</h4>
-                    <input className="inputfields" type='text' placeholder="Enter your Discription" required ref="textInput"></input><br />
-                    <input className="inputfields" type='integer' placeholder='Enter your Amount' required ref="amount" ></input><br />
-                    <input className="inputfields submit" type='submit' onClick={this.submitbtnclick}></input><br />
+                    <input className="inputfields" type='text' placeholder="Enter your Discription"  required onChange={(e)=>{textInput=e.target.value}} ></input><br />
+                    <input className="inputfields" type='integer' placeholder='Enter your Amount' required  onChange={(e)=>{amount=e.target.value}} ></input><br />
+                    <input className="inputfields submit" type='submit' onClick={submitbtnclick}></input><br />
                 </div>
                 </div>
             </div>
@@ -64,5 +83,5 @@ class  Home extends React.Component{
         )
         
     }
-}
+
 export default Home
--- src/addHistroy.js
+++ src/addHistroy.js
@@ -1,13 +1,24 @@
 import React from 'react'
-
-class addHistroy extends React.Component{
-    render(){
+import './addHistroy.css'
+function addHistroy({TransactionList,SetTransactionList, item, desc, amount,index}) {
+    const handleRemoveItem = e => {
+        const name = e.target.getAttribute("name")
+        console.log(e.target.name)
+        console.log("i don't know",TransactionList.slice(TransactionList.indexOf(item, 1)))
+        SetTransactionList(TransactionList.filter(item => item.desc !== name))
+    }
         return(
-            <div>
-                <li><span>{this.props.discription}</span> <span>{this.props.amount}</span></li>
+            <div className="addHistroy">
+                <div className="container">
+                    <div className="box1">{desc}
+                    </div>
+                    <div className="box2">{amount}
+                    </div>
+                    {/* <span className="delete" name={desc} onClick={handleRemoveItem}>X</span> */}
+                </div>
             </div>
            )
     }
     
-}
+
 export default addHistroy
